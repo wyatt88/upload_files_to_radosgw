3a3141efd26601b15ea755f564e9819d1cc22200
wenwen <wenwen@pingcap.com>
2018-11-27 17:24:45 +0800

    add args

--- upload_files_to_radosgw.py
+++ upload_files_to_radosgw.py
@@ -2,32 +2,45 @@ import mimetypes
 import boto.s3.connection
 from boto.s3.connection import S3Connection
 import os.path
+import argparse
 
-root_dir = ''
-access_key = ''
-secret_key = ''
-host = ''
-port = 0
-deploy_bucket_name = ''
+parser = argparse.ArgumentParser()
+parser.add_argument('--root_dir',required=True,help="path to static resource on local machine")
+parser.add_argument('--access_key',required=True,help="Ceph OBJ Storage Access Key")
+parser.add_argument('--secret_key',required=True,help="Ceph OBJ Storage Secret Key")
+parser.add_argument('--host',required=True,help="Ceph OBJ Storage Endpoint host")
+parser.add_argument('--port',required=True,help="Ceph OBJ Storage Endpoint port",default=8080)
+parser.add_argument('--deploy_bucket_name',required=True,help="Ceph OBJ Storage Bucket Name")
+
+parser.print_help()
+args = parser.parse_args()
+
+# root_dir = ''
+# access_key = ''
+# secret_key = ''
+# host = ''
+# port = 0
+# deploy_bucket_name = ''
+
 str_len = len(root_dir)
 
 conn = S3Connection(
-    aws_access_key_id=access_key,
-    aws_secret_access_key=secret_key,
-    host=host,
-    port=port,
+    aws_access_key_id=args.access_key,
+    aws_secret_access_key=args.secret_key,
+    host=args.host,
+    port=args.port,
     is_secure=False,  # uncomment if you are not using ssl
     calling_format=boto.s3.connection.OrdinaryCallingFormat(),
 )
 # 如果没有 <deploy_bucket> 则创建一个
 try:
-    bucket = conn.get_bucket(deploy_bucket_name)
+    bucket = conn.get_bucket(args.deploy_bucket_name)
 except:
-    bucket = conn.create_bucket(deploy_bucket_name)
+    bucket = conn.create_bucket(args.deploy_bucket_name)
 
 bucket.set_canned_acl('public-read')
 # 从 root_dir 上传所有文件，并按照目录/文件名的方式组合成 key,key 的 mime type 由 headers 决定
-for parent,dirnames,filenames in os.walk(root_dir):
+for parent,dirnames,filenames in os.walk(args.root_dir):
     for filename in filenames:
         key = bucket.new_key(os.path.join(parent[str_len+1:],filename))
         contentType = mimetypes.guess_type(os.path.join(parent,filename))
